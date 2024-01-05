231372e9c62c21b049229f302b1de2a130622209
hoan-swe <152995318+hoan-swe@users.noreply.github.com>
2024-01-05 21:00:42 +0000

    Improve Solving Algorithm and Fix Bugs.
    Fix bugs in Algorithm where in some cases the program solve incorrectly.
    Add more layers to Algorithm. The program is able to solve ALL easy problems, and MOST medium problems.
    Put some checks in place to skip unnecessary/redundant steps in Algorithm.

--- ValidCheck.py
+++ ValidCheck.py
@@ -2,23 +2,36 @@
 # whether any assigned entry
 # in the specified row matches 
 # the given number.
-def Is_in_row(grid, row, num):
-  return any(grid[row][i] == num for i in range(9))
+'''def Is_in_row(grid, row, num):
+  return any(grid[row][i] == num for i in range(9))'''
 
 # Returns a boolean which indicates 
 # whether any assigned entry
 # in the specified column matches 
 # the given number.
-def Is_in_col(grid, col, num):
-  return any(grid[i][col] == num for i in range(9))
+'''def Is_in_col(grid, col, num):
+  return any(grid[i][col] == num for i in range(9))'''
 
 # Returns a boolean which indicates 
 # whether any assigned entry
 # within the specified 3x3 box 
 # matches the given number
-def Is_in_box(grid, row, col, num):
+'''def Is_in_box(grid, row, col, num):
     for i in range(3):
         for j in range(3):
             if(grid[i + row][j + col] == num):
                 return True
+    return False'''
+
+
+def Is_used(grid, row, col, num):
+  if any(grid[row][i] == num for i in range(9)) or any(grid[i][col] == num for i in range(9)):
+    return True
+  else:
+    row = row - row%3
+    col = col - col%3
+    for i in range(3):
+      for j in range(3):
+        if(grid[i + row][j + col] == num):
+          return True
     return False
